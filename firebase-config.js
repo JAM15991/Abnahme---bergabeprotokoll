475391bd749b5bb367f54e9369d6ace457c4dd4b
JAM15991 <j_A_m@hotmail.de>
2025-10-30 11:54:27 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyClU4cDJO4SM9e0NW-PlfjDQnO5K2Q9js8",
+  authDomain: "wohnungsprotokoll-8eefd.firebaseapp.com",
+  projectId: "wohnungsprotokoll-8eefd",
+  storageBucket: "wohnungsprotokoll-8eefd.firebasestorage.app",
+  messagingSenderId: "914641881389",
+  appId: "1:914641881389:web:3c4ddaeeaa514d33372ba4",
+  measurementId: "G-P4VF34H1K8"
+};
